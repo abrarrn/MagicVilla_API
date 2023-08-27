8d252ec42b7358fbacd0f6e489e705089e268f09
Abrar Rafid Noor <arn.official@yahoo.com>
2023-08-27 22:03:02 +0600

    Written an end point

---
+++ WebApplication1/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController: ControllerBase
+    {
+        /*Endpoint in API is something that accepts requests and sends back response*/
+        [HttpGet]   //an end point
+        public IEnumerable<Villa> GetVillas() { 
+            return new List<Villa>
+            {
+                new Villa{Id=1, Name="Pool View"},
+                new Villa{Id=2, Name="Beach View"}
+            };
+        }
+    }
+}
---
+++ WebApplication1/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Model
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
