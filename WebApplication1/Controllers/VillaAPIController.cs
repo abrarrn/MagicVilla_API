36cdd89fa6bc9fa68c3158d404077edcd956e7c1
Abrar Rafid Noor <arn.official@yahoo.com>
2023-08-27 22:27:31 +0600

    GetVilla implemented

--- WebApplication1/Controllers/VillaAPIController.cs
+++ WebApplication1/Controllers/VillaAPIController.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
 using WebApplication1.Model;
+using WebApplication1.Models.Dto;
 
 namespace WebApplication1.Controllers
 {
-    [Route("api/VillaAPI")]
+    [Route("api/VillaAPI")] //can also be used as [Route("api/[controller]")]
     [ApiController]
     public class VillaAPIController: ControllerBase
     {
         /*Endpoint in API is something that accepts requests and sends back response*/
         [HttpGet]   //an end point
-        public IEnumerable<Villa> GetVillas() { 
-            return new List<Villa>
-            {
-                new Villa{Id=1, Name="Pool View"},
-                new Villa{Id=2, Name="Beach View"}
-            };
+        public IEnumerable<VillaDTO> GetVillas() {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
         }
     }
 }
---
+++ WebApplication1/Models/Dto/VillaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models.Dto
+{
+    /*data transfer object (DTO) is an object that carries data between processes
+     a wrapper to hide the model or database from being exposed in api*/
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
